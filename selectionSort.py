c6dc7e0bf276c46bb6994c515788ea912dc89f9e
LuixCabral <134115618+LuixCabral@users.noreply.github.com>
2024-06-12 15:20:14 -0300

    alteração de erro na escrita do codigo

--- selectionSort.py
+++ selectionSort.py
@@ -2,7 +2,7 @@ arrai = [64, 34, 25, 5, 22, 11, 90, 12]
 
 n = len(arrai)
 
-for i in range(n-1):
+for i in range(n):
     index_Minimo= 1
     for j in range(i+1,n):
         if arrai[j] < arrai[index_Minimo]:
